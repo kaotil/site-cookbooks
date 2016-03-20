01cbc734a20692af24e733445d66969309574038
kaotil <kaori.maeda@gmail.com>
2016-03-20 21:12:51 +0900

    nginx set up

---
+++ web/recipes/default.rb
@@ -0,0 +1,19 @@
+package "nginx" do
+  action :install
+end
+
+service "nginx" do
+  supports status: true, restart: true, reload: true
+  action [:enable, :start]
+end
+
+# ./site_cookbooks/templates/default/nginx.conf.erbを元にして
+# nginxの設定ファイルを決まったところに置くよという指示
+# Chefの規約にのおかげで置き場所のパスやテンプレートファイルは省略できている
+template "kaotil.com.conf" do
+  owner "root"
+  group "root"
+  mode 0644
+
+  notifies :reload, "service[nginx]"
+end
