8aff598bc6a7c6935b3f1a0a971c7b1c85c88f0b
kaotil <kaori.maeda@gmail.com>
2016-04-03 19:42:36 +0900

    タイムゾーン変更レシピ

---
+++ system/recipes/timezone.rb
@@ -0,0 +1,8 @@
+template "/etc/sysconfig/clock" do
+  notifies :run, "execute[timezone_update]", :immediately
+end
+
+execute "timezone_update" do
+  command "cp /usr/share/zoneinfo/Japan /etc/localtime"
+  action :nothing
+end
